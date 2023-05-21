1eaf315f7b6626cf324aac4ea65d42c166f11728
Hayk-PG <pautikgames@gmail.com>
2023-05-21 03:26:39 -0400

    Set conveyor base active/inactive based on boolean parameter

--- Assets/NEW/Scripts/SelectableObjects/Conveyors/ConveyorReplacementManager.cs
+++ Assets/NEW/Scripts/SelectableObjects/Conveyors/ConveyorReplacementManager.cs
@@ -1,9 +1,13 @@
+using System.Collections;
 using UnityEngine;
 
 public class ConveyorReplacementManager : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField] private SelectableObjectInfo _selectableObjectInfo;
+
+    [Header("Conveyor Base Transform")]
+    [SerializeField] private Transform _conveyorBase;
 
 
 
@@ -19,5 +23,36 @@ public class ConveyorReplacementManager : MonoBehaviour
         References.Manager.ObjectPlacementValidator.RaiseObjectPlacementValidationRequestEvent(specifiedTilePosition, _selectableObjectInfo);
         References.Manager.ObjectPlacementValidator.PlaceSelectedObject();
         References.Manager.ConveyorCollection.Add(transform.position, this);
+        StartCoroutine(RunAdjacentSidesCheck());
+    }
+
+    private IEnumerator RunAdjacentSidesCheck()
+    {
+        yield return new WaitForSeconds(0.1f);
+        CheckConveyorsOnAdjacentSides();
+    }
+
+    private void CheckConveyorsOnAdjacentSides()
+    {
+        bool haveConveyorsOnAdjacentSides = References.Manager.ConveyorCollection.Dict.ContainsKey(AdjacentPositions()[0]) && References.Manager.ConveyorCollection.Dict.ContainsKey(AdjacentPositions()[1]) ||
+                                            References.Manager.ConveyorCollection.Dict.ContainsKey(AdjacentPositions()[2]) && References.Manager.ConveyorCollection.Dict.ContainsKey(AdjacentPositions()[3]);
+
+        SetConveyorBaseActive(!haveConveyorsOnAdjacentSides);
+    }
+
+    private Vector3[] AdjacentPositions()
+    {
+        return new Vector3[]
+        {
+            transform.position + Vector3.right,
+            transform.position + Vector3.left,
+            transform.position + Vector3.forward,
+            transform.position + Vector3.back
+        };
+    }
+
+    private void SetConveyorBaseActive(bool isActive)
+    {
+        _conveyorBase.gameObject.SetActive(isActive);
     }
 }
