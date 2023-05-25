6d401649c8732c9659552d5ba12228baa58a7bb2
Hayk-PG <pautikgames@gmail.com>
2023-05-25 02:56:39 -0400

    Fixed the bug causing items to stop moving on newly created conveyor segments

--- Assets/NEW/Scripts/ItemObjects/ItemManager.cs
+++ Assets/NEW/Scripts/ItemObjects/ItemManager.cs
@@ -17,6 +17,7 @@ public class ItemManager : MonoBehaviour
 
     private bool _isMoved; // Flag indicating if the item is currently being moved.
     private List<MoveData> _queuedMoves = new List<MoveData>(); // Queue of queued movement data.
+    private ConveyorSegment _targetConveyorSegment;
 
 
 
@@ -52,22 +53,60 @@ public class ItemManager : MonoBehaviour
         // Calculate the initial distance and distance threshold
         float distance = Vector3.Distance(initialPosition, targetPosition);
         float distanceThreshold = 0.05f;
+        float elapsedTime  = 0f;
         bool canMove = false;
 
         // Check if the target conveyor position exists in the dictionary
-        Checker.IsValueInDictionary(References.Manager.ConveyorCollection.Dict, targetConveyorPosition, out ConveyorSegment targetConveyorSegment);
+        Checker.IsValueInDictionary(References.Manager.ConveyorCollection.Dict, targetConveyorPosition, out _targetConveyorSegment);
 
         // Move towards the target position while the distance is above the threshold
         while (distance > distanceThreshold)
         {
-            if (targetConveyorSegment != null && !targetConveyorSegment.ConveyorTrigger.HasTriggeredItem || canMove)
+            bool shouldMove = _targetConveyorSegment != null && !_targetConveyorSegment.ConveyorTrigger.HasTriggeredItem || canMove;
+
+            // Determine if the conveyor should move based on the target conveyor segment and item trigger conditions, or if movement is explicitly allowed.
+            if (shouldMove)
             {
                 canMove = true;
                 // Calculate the lerped position based on the current time and fixed delta time
                 lerpedPosition = Vector3.Lerp(_rigidbody.position, targetPosition, time * Time.fixedDeltaTime);
                 // Update the distance based on the new position
                 distance = Vector3.Distance(_rigidbody.position, targetPosition);
-                MoveToPosition(lerpedPosition);
+                MoveToPosition(lerpedPosition);             
+            }
+            else
+            {
+                elapsedTime  += Time.fixedDeltaTime;
+
+                // Check if the specified time has passed
+                if (elapsedTime  >= 1f)
+                {
+                    // Iterate over the adjacent positions of the conveyor segment
+                    for (int i = 0; i < AdjacentPositionCalculator.GetAdjacentPositions(conveyorSegment.transform.position).Length; i++)
+                    {
+                        Vector3 randomConveyorPosition = AdjacentPositionCalculator.GetAdjacentPositions(conveyorSegment.transform.position)[i];
+
+                        // Check if the adjacent position corresponds to a just created conveyor segment
+                        Checker.IsValueInDictionary(References.Manager.ConveyorCollection.Dict, randomConveyorPosition, out ConveyorSegment justCreatedConveyorSegment);
+
+                        // Check if there is no target conveyor segment and a just created conveyor segment exists
+                        if (_targetConveyorSegment == null && justCreatedConveyorSegment != null)
+                        {
+                            bool canMoveItem = !justCreatedConveyorSegment.ConveyorTrigger.HasTriggeredItem;
+                            bool hasDifferentPosition = conveyorSegment.transform.position + -direction != justCreatedConveyorSegment.transform.position;
+
+                            // Check if the just created conveyor segment is eligible for moving an item
+                            if (canMoveItem && hasDifferentPosition)
+                            {
+                                // Update the target conveyor segment and set the new target position.
+                                _targetConveyorSegment = justCreatedConveyorSegment;
+                                targetPosition = new Vector3(_targetConveyorSegment.transform.position.x, initialPosition.y, _targetConveyorSegment.transform.position.z);                              
+                            }                           
+                        }                        
+                    }
+
+                    elapsedTime  = 0f;
+                }
             }
 
             yield return new WaitForSeconds(Time.fixedDeltaTime);
